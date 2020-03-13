730a0bb908ff71906e70c22217e2a9c3a7bbd048
zhangyi <loeyae@gmail.com>
2020-03-13 20:00:37 +0800

    优化文件

--- src/main/java/com/loeyae/tools/es_utils/component/ElasticSearchIndicesUtils.java
+++ src/main/java/com/loeyae/tools/es_utils/component/ElasticSearchIndicesUtils.java
@@ -45,105 +45,99 @@ public class ElasticSearchIndicesUtils {
     /**
      * 创建索引
      *
-     * @param name
-     * @return
+     * @param name index name
+     * @return true | false
      */
     public boolean createIndex(String name) {
         CreateIndexRequest createIndexRequest = buildCreateIndexRequest(name);
         buildIndexSetting(createIndexRequest, null);
         CreateIndexResponse createIndexResponse = doCreateIndex(createIndexRequest);
-        boolean restStatus = filterCreateIndexResponse(createIndexResponse);
-        return restStatus;
+        return filterCreateIndexResponse(createIndexResponse);
     }
 
     /**
      * 创建索引
      *
-     * @param name
-     * @param settings
-     * @return
+     * @param name     index name
+     * @param settings Array of Integer
+     * @return true | false
      */
     public boolean createIndex(String name, Integer[] settings) {
         CreateIndexRequest createIndexRequest = buildCreateIndexRequest(name);
         buildIndexSetting(createIndexRequest, settings);
         CreateIndexResponse createIndexResponse = doCreateIndex(createIndexRequest);
-        boolean restStatus = filterCreateIndexResponse(createIndexResponse);
-        return restStatus;
+        return filterCreateIndexResponse(createIndexResponse);
     }
 
     /**
      * 创建索引
      *
-     * @param name
-     * @param type
-     * @param fields
-     * @return
+     * @param name   index name
+     * @param type   doc type
+     * @param fields Map of fields
+     * @return true | false
      */
     public boolean createIndex(String name, String type, Map<String, Object> fields) {
         CreateIndexRequest createIndexRequest = buildCreateIndexRequest(name);
         buildIndexSetting(createIndexRequest, null);
         buildIndexMapping(createIndexRequest, type, fields);
         CreateIndexResponse createIndexResponse = doCreateIndex(createIndexRequest);
-        boolean restStatus = filterCreateIndexResponse(createIndexResponse);
-        return restStatus;
+        return filterCreateIndexResponse(createIndexResponse);
     }
 
     /**
      * 创建索引
      *
-     * @param name
-     * @param fields
-     * @return
+     * @param name   index name
+     * @param fields Map of fields
+     * @return true | false
      */
     public boolean createIndex(String name, Map<String, Object> fields) {
         CreateIndexRequest createIndexRequest = buildCreateIndexRequest(name);
         buildIndexSetting(createIndexRequest, null);
         buildIndexMapping(createIndexRequest, DEFAULT_INDEX_TYPE, fields);
         CreateIndexResponse createIndexResponse = doCreateIndex(createIndexRequest);
-        boolean restStatus = filterCreateIndexResponse(createIndexResponse);
-        return restStatus;
+        return filterCreateIndexResponse(createIndexResponse);
     }
 
     /**
      * 创建索引
      *
-     * @param name
-     * @param settings
-     * @param type
-     * @param fields
-     * @return
+     * @param name     index name
+     * @param settings Array of Integer
+     * @param type     doc type
+     * @param fields   Map of fields
+     * @return true | false
      */
     public boolean createIndex(String name, Integer[] settings, String type, Map<String, Object> fields) {
         CreateIndexRequest createIndexRequest = buildCreateIndexRequest(name);
         buildIndexSetting(createIndexRequest, settings);
         buildIndexMapping(createIndexRequest, type, fields);
         CreateIndexResponse createIndexResponse = doCreateIndex(createIndexRequest);
-        boolean restStatus = filterCreateIndexResponse(createIndexResponse);
-        return restStatus;
+        return filterCreateIndexResponse(createIndexResponse);
     }
 
     /**
      * 创建索引
      *
-     * @param name
-     * @param settings
-     * @param fields
-     * @return
+     * @param name     index name
+     * @param settings Array of Integer
+     * @param fields   Map of fields
+     * @return true | false
      */
     public boolean createIndex(String name, Integer[] settings, Map<String, Object> fields) {
         CreateIndexRequest createIndexRequest = buildCreateIndexRequest(name);
         buildIndexSetting(createIndexRequest, settings);
         buildIndexMapping(createIndexRequest, DEFAULT_INDEX_TYPE, fields);
         CreateIndexResponse createIndexResponse = doCreateIndex(createIndexRequest);
-        boolean restStatus = filterCreateIndexResponse(createIndexResponse);
-        return restStatus;
+        return filterCreateIndexResponse(createIndexResponse);
     }
 
     /**
      * 索引是否存在
      *
-     * @param name
-     * @return
+     * @param name index name
+     * @return true | false
      */
     public boolean indexExists(String name) {
         boolean restStatus = false;
@@ -161,8 +155,8 @@ public class ElasticSearchIndicesUtils {
     /**
      * 删除索引
      *
-     * @param name
-     * @return
+     * @param name index name
+     * @return true | false
      */
     public boolean deleteIndex(String name) {
         boolean restStatus = false;
@@ -184,8 +178,8 @@ public class ElasticSearchIndicesUtils {
     /**
      * 分析创建索引结果
      *
-     * @param createIndexResponse
-     * @return
+     * @param createIndexResponse instance of CreateIndexResponse
+     * @return true | false
      */
     protected boolean filterCreateIndexResponse(CreateIndexResponse createIndexResponse) {
         if (null == createIndexResponse) {
@@ -197,8 +191,8 @@ public class ElasticSearchIndicesUtils {
     /**
      * 执行创建索引操作
      *
-     * @param createIndexRequest
-     * @return
+     * @param createIndexRequest instance of CreateIndexRequest
+     * @return instance of CreateIndexResponse
      */
     protected CreateIndexResponse doCreateIndex(CreateIndexRequest createIndexRequest) {
         CreateIndexResponse createIndexResponse = null;
@@ -215,23 +209,21 @@ public class ElasticSearchIndicesUtils {
     /**
      * 构造CreateIndexRequest
      *
-     * @param name 索引名称
-     * @return
+     * @param name index name
+     * @return instance of CreateIndexRequest
      */
     protected CreateIndexRequest buildCreateIndexRequest(String name) {
         assert StringUtils.isNotEmpty(name);
-        CreateIndexRequest createIndexRequest = new CreateIndexRequest(name);
-        return createIndexRequest;
+        return new CreateIndexRequest(name);
     }
 
     /**
      * 构造index setting
      *
-     * @param indexRequest
-     * @param settings
+     * @param indexRequest instance of CreateIndexRequest
+     * @param settings Array of Integer
      */
-    protected void buildIndexSetting(CreateIndexRequest indexRequest,
-                                     Integer[] settings) {
+    protected void buildIndexSetting(CreateIndexRequest indexRequest, Integer[] settings) {
         Integer shards = INDEX_DEFAULT_SHARDS;
         Integer replicas = INDEX_DEFAULT_REPLICAS;
         if (null != settings) {
@@ -248,9 +240,10 @@ public class ElasticSearchIndicesUtils {
 
     /**
      * 构造mapping
-     * @param createIndexRequest
-     * @param type
-     * @param fields
+     *
+     * @param createIndexRequest instance of CreateIndexRequest
+     * @param type               doc type
+     * @param fields             Map of fields
      */
     protected void buildIndexMapping(CreateIndexRequest createIndexRequest, String type, Map<String,
             Object> fields) {
@@ -258,53 +251,46 @@ public class ElasticSearchIndicesUtils {
         try {
             builder = XContentFactory.jsonBuilder();
             builder.startObject();
-            {
-                builder.startObject(type);
-                {
-                    builder.startObject("properties");
-                    {
-                        buildIndexFields(builder, fields);
-                    }
-                    builder.endObject();
-                }
-                builder.endObject();
-            }
+            builder.startObject(type);
+            builder.startObject("properties");
+            buildIndexFields(builder, fields);
+            builder.endObject();
+            builder.endObject();
             builder.endObject();
         } catch (IOException e) {
             log.error(DEFAULT_ERROR_MSG, e);
         }
+        assert builder != null;
         createIndexRequest.mapping(type, builder);
     }
 
     /**
      * 构造字段集
      *
-     * @param builder
-     * @param fields
+     * @param builder instance of XContentBuilder
+     * @param fields  Map of fields
      */
-    protected void buildIndexFields(XContentBuilder builder,
-                                    Map<String, Object> fields) {
+    @SuppressWarnings("unchecked")
+    protected void buildIndexFields(XContentBuilder builder, Map<String, Object> fields) {
         fields.forEach((key, value) -> {
             try {
                 builder.startObject(key);
-                {
-                    if (value instanceof Map) {
-                        ((Map) value).forEach((k, v)->{
-                            if (v instanceof Map) {
-                                buildIndexFields(builder, new HashMap<String, Object>(){{
-                                    put(String.valueOf(k), v);
-                                }});
-                            } else {
-                                try {
-                                    builder.field(String.valueOf(k), v);
-                                } catch (Exception e) {
-                                    log.error(DEFAULT_ERROR_MSG, e);
-                                }
+                if (value instanceof Map) {
+                    ((Map<Object, Object>) value).forEach((k, v)->{
+                        if (v instanceof Map) {
+                            Map<String, Object> map = new HashMap<>();
+                            map.put(String.valueOf(k), v);
+                            buildIndexFields(builder, map);
+                        } else {
+                            try {
+                                builder.field(String.valueOf(k), v);
+                            } catch (Exception e) {
+                                log.error(DEFAULT_ERROR_MSG, e);
                             }
-                        });
-                    } else {
-                        builder.field("type", value);
-                    }
+                        }
+                    });
+                } else {
+                    builder.field("type", value);
                 }
                 builder.endObject();
             } catch (IOException e) {
